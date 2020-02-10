3777c030dd39546421684fecae22ab2a740be472
Hemant Verma <hemant.verma@nutanix.com>
2020-02-10 12:57:59 +0530

    initial commit

---
+++ src/main/java/com/nutanix/hack/seeksmart/SeeksmartApplication.java
@@ -0,0 +1,13 @@
+package com.nutanix.hack.seeksmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeksmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeksmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutanix/hack/seeksmart/SeeksmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutanix.hack.seeksmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeksmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
