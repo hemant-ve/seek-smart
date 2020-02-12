d69b0c9b1d2f813c3ce10b397bcc00d3e17d8563
bhavathi.reddy <bhavathi.reddy@nutanix.com>
2020-02-12 18:54:59 +0530

    Adding id to post item

--- src/main/java/com/nutanix/hack/seeksmart/pojo/response/PostItem.java
+++ src/main/java/com/nutanix/hack/seeksmart/pojo/response/PostItem.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostItem {
+    private Long id;
     private String rant;
     private Long createdAt;
     private Integer noOfAcks;
