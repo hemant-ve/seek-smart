575e559f7d14345df2e2c3ed0bc28a3f61412911
bhavathi.reddy <bhavathi.reddy@nutanix.com>
2020-02-12 14:18:27 +0530

    Bhavathi: Repositories added

--- src/main/java/com/nutanix/hack/seeksmart/repository/SentimentRepository.java
+++ src/main/java/com/nutanix/hack/seeksmart/repository/SentimentRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SentimentRepository extends JpaRepository<Sentiment, Long> {
 
     List<Sentiment> findByTagEqualsAndTimestampBetween(int tag, Long startTime, Long endTime);
-    Sentiment findTopByTagOrderByTimestampDesc(int tag);
+    Optional<Sentiment> findTopByTagOrderByTimestampDesc(int tag);
 }
